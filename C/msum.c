f87cdb888d7448669d3484c6105a07bbc0883ecf
ananthanandanan <ananthanandanan@gmail.com>
2020-04-26 16:40:21 +0530

    C: modified msum.c

--- C/msum.c
+++ C/msum.c
@@ -2,23 +2,32 @@
 
 int main(){
 
-    int a[3][3],b[3][3];
+    int r=0,c=0;
 
     //first mat
-    for(int i=0; i<3;i++){
+    printf("enter the row and column:\n");
+    scanf("%d",&r);
+    scanf("%d",&c);
+
+    int a[r][c], b[r][c];
+    printf("enter the elements of a\n ");
+
+
+    for(int i=0; i<r;i++){
         
-        for(int j=0;j<3;j++){
+        for(int j=0;j<c;j++){
             scanf("%d",&a[i][j]);
             
         }
         printf("\n");
 
     }
+    printf("enter the elements of b\n ");
     //sec mat
-    for(int i=0; i<3;i++){
+    for(int i=0; i<r;i++){
         
-        for(int j=0;j<3;j++){
-            scanf("%d",&a[i][j]);
+        for(int j=0;j<c;j++){
+            scanf("%d",&b[i][j]);
             
         }
         printf("\n");
@@ -26,15 +35,16 @@ int main(){
     }
     //print sum of a and b
 
-    for(int i=0; i<3;i++){
+    for(int i=0; i<r;i++)
+    {
         printf("\n");
-        for(int j=0;j<3;j++)
+        for(int j=0;j<c;j++)
         printf("%d ",a[i][j]+b[i][j]);
     }
         // diff of a and b
-    for(int i=0; i<3;i++){
+    for(int i=0; i<r;i++){
         printf("\n");
-        for(int j=0;j<3;j++)
+        for(int j=0;j<c;j++)
         printf("%d ",a[i][j]-b[i][j]);
     }
 
