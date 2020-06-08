b25f64c8fecba3484e8067ce18a77e93cbb67564
ananthanandanan <ananthanandanan@gmail.com>
2020-06-08 23:33:23 +0530

    foss challenge-1

---
+++ C/foss_clng/lisa.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include <math.h>
+
+
+int main() {
+
+    long int n;
+    scanf("%ld",&n);
+    int cnt = 0; 
+    for (int i = 1; i <= sqrt(n); i++) 
+    { 
+        if (n % i == 0)
+         { 
+            // If divisors are equal, 
+            // count only one 
+            if (n / i == i) 
+                cnt++; 
+  
+            else // Otherwise count both 
+                cnt = cnt + 2; 
+        } 
+    }
+    printf("%d",cnt);
+    return 0;
+}
---
+++ UI/blog/login.js
@@ -0,0 +1,27 @@
+let count = 0;
+
+function login()
+{
+	count++;
+	if(count>1)window.location.href = "home.html";
+}
+$(function() {
+
+    $('#login-form-link').click(function(e) {
+		$("#login-form").delay(100).fadeIn(100);
+ 		$("#register-form").fadeOut(100);
+		$('#register-form-link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+		count++;
+		if(count==1)location.replace("/home.html");
+	});
+	$('#register-form-link').click(function(e) {
+		$("#register-form").delay(100).fadeIn(100);
+ 		$("#login-form").fadeOut(100);
+		$('#login-form-link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+
+});
