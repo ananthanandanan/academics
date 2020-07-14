cc4819e24c5f628f42c2a28816624ef482608107
ananthanandanan <ananthanandanan@gmail.com>
2020-07-15 01:09:54 +0530

    delete blog

--- UI/blog/blog.js
+++
@@ -1,9 +0,0 @@
-function openSlideMenu(){
-    document.getElementById('side-menu').style.width = '250px';
-    document.getElementById('main').style.marginLeft = '250px';
-  }
-
-  function closeSlideMenu(){
-    document.getElementById('side-menu').style.width = '0';
-    document.getElementById('main').style.marginLeft = '0';
-  }
--- UI/blog/readme.md
+++
@@ -1,4 +0,0 @@
-## Personal Blog from Scratch
-
-- For UI assignment
-- Uses HTML, CSS, Javascript, Bootstrap, Jquery
