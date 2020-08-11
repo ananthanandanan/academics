1e66d3143734c1ce7a87dffe2a12867a24e9f11e
ananthanandanan <ananthanandanan@gmail.com>
2020-08-12 01:31:22 +0530

    http hello server

---
+++ Networking/py_soc/HTTP-web/http_server.py
@@ -0,0 +1,22 @@
+
+
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+
+class HelloHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # First, send a 200 OK response.
+        self.send_response(200)
+
+        # Then send headers.
+        self.send_header('Content-type', 'text/plain; charset=utf-8')
+        self.end_headers()
+
+        # Now, write the response body.
+        self.wfile.write("Hello, HTTP!\n".encode())
+
+if __name__ == '__main__':
+    server_address = ('', 8000)  # Serve on all addresses, port 8000.
+    httpd = HTTPServer(server_address, HelloHandler)
+    httpd.serve_forever()
